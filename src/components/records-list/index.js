8d51222ba559f27382932e6fb5b24cb98fa4879c
shimkonaz <shimkonaz@gmail.com>
2020-09-10 20:14:28 +0300

    add records list component

---
+++ src/components/records-list/index.js
@@ -0,0 +1,3 @@
+import RecordsList from './records-list.component';
+
+export { RecordsList };
---
+++ src/components/records-list/records-list.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class RecordsList extends Component {
+  render() {
+    return (
+      <div className="records-list">
+        <h2>
+          Records List
+        </h2>
+      </div>
+    )
+  }
+}
